292c3ffd6a838ff79ff80d3baef7a10d9cd91421
robertosilvatim <roberto.silva.ti@gmail.com>
2023-12-07 17:11:32 -0300

    modified files: urls.py /home.html

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,6 @@ import crud
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='crud.html')),
-    path('other-home/', TemplateView.as_view(template_name='home.html')),
-    path('crud/', include('crud.urls')),
+    path('home/', TemplateView.as_view(template_name='home.html')),
+    path('', include('crud.urls')),
 ]
